7dc540dcec65b73c8b2b9622faac9bd939873a30
zhuixing05 <36839600+zhuixing05@users.noreply.github.com>
2024-08-30 20:45:11 +0700

    Update i18n.js

--- i18n/i18n.js
+++ i18n/i18n.js
@@ -1,12 +1,12 @@
 import {createI18n} from "vue-i18n";
-import zh from './ja.js'
+import zh from './zh.js'
 
 const i18n = createI18n({
     legacy: false, 
-    locale: 'ja', // 默认语言
+    locale: 'zh', // 默认语言
     globalInjection: true, // 全局注册$t方法
     messages: {
-        ja,
+        zh,
     }
 })
 
